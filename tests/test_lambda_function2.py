ac68473e0486de7eb708598f7583208166a193f5
Damian Ziobro <damian@xmementoit.com>
2021-11-12 14:23:53 +0000

    reformat test code according to new isort and black rules

--- tests/test_lambda_function2.py
+++ tests/test_lambda_function2.py
@@ -1,15 +1,16 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
+"""Test_lambda_function2."""
 
 import unittest
 
 from app import lambda_function2
 
+
 class TestHelloModule(unittest.TestCase):
+    """TestsHelloModules."""
 
     def test_lambda_function1_returns_success(self):
+        """Check that lambda_function1 returns success."""
         test_message = "test_message_text"
-        event = {'Records': [{"body": test_message}]}
+        event = {"Records": [{"body": test_message}]}
         result = lambda_function2.lambda_handler(event, None)
         self.assertEqual(test_message, result)
